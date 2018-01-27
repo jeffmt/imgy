06e158d946b020739d0740da1640457a8be342eb
Jeff T <tarutmauktoe@gmail.com>
2018-01-27 14:06:43 -0600

    Initial commit

---
+++ src/main/java/com/example/imgy/ImgyApplication.java
@@ -0,0 +1,12 @@
+package com.example.imgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgyApplication.class, args);
+	}
+}
