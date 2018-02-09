5ef2d2cfc0d2a0a1a6f005d6c8a6c3067f0fd412
Jeff T <tarutmauktoe@gmail.com>
2018-02-09 13:11:14 -0600

    Able to create a post via REST interface

--- src/main/java/com/example/imgy/controllers/PostRestController.java
+++ src/main/java/com/example/imgy/controllers/PostRestController.java
@@ -4,6 +4,8 @@ import com.example.imgy.models.Post;
 import com.example.imgy.models.data.PostDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -11,11 +13,23 @@ import java.util.List;
 @RestController
 public class PostRestController {
 
-//    @Autowired
-//    private PostDAO postDao;
+    @Autowired
+    private PostDAO postDao;
 
-//    @GetMapping("/posts")
-//    public Iterable<Post> retrieveAllPosts() {
-//        return postDao.findAll();
-//    }
+    @GetMapping("/posts")
+    public Iterable<Post> retrieveAllPosts() {
+//    public List<Post> retrieveAllPosts() {
+        System.out.println("In here yall");
+
+
+//        Iterable<Post> posts = postDao.findAll();
+
+
+        return postDao.findAll();
+    }
+
+    @PostMapping("/posts")
+    public void createPost(@RequestBody Post post) {
+        Post createdPost = postDao.save(post);
+    }
 }
