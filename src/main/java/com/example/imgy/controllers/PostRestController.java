0c292182093ebc2f5a06f694b4d38c59ac6278aa
Jeff T <tarutmauktoe@gmail.com>
2018-02-20 15:20:22 -0600

    Add CrossOrigin to enable react app from localhost:3000 to make ajax calls to rest api at localhost:8095

--- src/main/java/com/example/imgy/controllers/PostRestController.java
+++ src/main/java/com/example/imgy/controllers/PostRestController.java
@@ -16,6 +16,7 @@ import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 
 import java.net.URI;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RestController
 @RequestMapping("posts")
 public class PostRestController {
